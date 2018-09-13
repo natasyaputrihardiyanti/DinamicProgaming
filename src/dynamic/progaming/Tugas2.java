6db806603fa6d89415d62d67ce11c8e21d3daeff
acer <acer@DESKTOP-DOEF5V7>
2018-09-13 08:22:17 +0700

    Identitas

---
+++ src/dynamic/progaming/Tugas2.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dynamic.progaming;
+
+
+
+/**
+ *
+ * @author acer
+ */
+public class Tugas2 {
+    
+    public static void main(String[]args){
+         String identitas = "Natasya Putri Hardiyanti / XRPL5 /30";
+    judul(identitas);
+    }
+   
+    public static void judul(String identitas){
+         System.out.println(" Nama Saya  : "+identitas);
+         
+         System.out.println("==JASA PENGANGKUT BARANG==");
+        
+      
+    }
+    
+}
+
